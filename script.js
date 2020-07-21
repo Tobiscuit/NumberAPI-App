c023da6afc24911632ffd3903944318a30c865ac
Tobiscuit <33387497+Tobiscuit@users.noreply.github.com>
2020-07-21 18:05:08 -0500

    Built the app from scratch. WIP

---
+++ script.js
@@ -0,0 +1,46 @@
+const numberInput = document.querySelector('form > label > input[type="number"]');
+const submitBtn = document.querySelector('input[type=submit]');
+const outputSection = document.querySelector('section.output');
+const typeInput = document.getElementById('type');
+let number = 42;
+let type = '';
+let NUMBERS_API = `http://numbersapi.com`;
+let textFrag = '';
+
+
+async function fetchData() {
+	let response = await fetch(`${NUMBERS_API}/${number}/${type}?json`)
+	let data = await response.json()
+	textFrag = await data.text;
+	//
+	let div = document.createElement('div');
+	div.appendChild(document.createTextNode(`${type}: ${textFrag}`));
+	outputSection.appendChild(div);
+}
+
+
+function generateResponse(e) {
+	number = numberInput.value;
+	type = typeInput.value;
+	
+}
+
+// function appendElement() {
+	
+// 	let div = document.createElement('div');
+// 	div.appendChild(document.createTextNode(`${number} ${type} ${textFrag}`));
+// 	outputSection.appendChild(div);
+// }
+
+submitBtn.addEventListener('click', function(e) {
+	e.preventDefault();
+	generateResponse(e);
+	fetchData();
+	// appendElement();
+	numberInput.value = '';
+})
+
+
+outputSection.onclick = function() {
+	console.log('Clicked on output section!')
+}
